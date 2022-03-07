6d58472a1395ab9a23a149bd34b3c0a1f5d09031
Augusto <augusto.cesar.99@hotmail.com>
2022-03-06 23:38:20 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <v-navigation-drawer
+        app
+        mini-variant
+        expand-on-hover
+        dark
+      >
+        <TheNavbar />
+      </v-navigation-drawer>
+      
+      <v-main>
+        <router-view style="border:1px solid red;" />
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import TheNavbar from './components/TheNavbar.vue'
+
+export default {
+  components: { TheNavbar }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-list>
+      <v-list-item class="px-2" link href="https://github.com/Augustotwz">
+        <v-list-item-avatar>
+          <v-icon>mdi-github</v-icon>
+        </v-list-item-avatar>
+        <v-list-item-title>GITHUB</v-list-item-title>
+      </v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list nav dense>
+      <v-list-item link to="/">
+        <v-list-item-icon>
+          <v-icon>mdi-folder</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>HOME</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item link to="/about">
+        <v-list-item-icon>
+          <v-icon>mdi-account-multiple</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>ABOUT</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
